37254b9cf12fb74193cb68f56f218cf191e86fff
juno <18845792124@163.com>
2018-12-03 21:04:51 +0800

    注册登陆界面完成

--- gzhipin-client/src/components/login/index.jsx
+++ gzhipin-client/src/components/login/index.jsx
@@ -1,9 +1,43 @@
 import React, {Component} from 'react';
+import {NavBar,WingBlank,WhiteSpace,List,InputItem,Button} from 'antd-mobile';
+import Logo from '../logo';
 
 class Login extends Component {
+    state = {
+        username:'',
+        password:''
+    }
+
+    onChange = (type,value) => {
+        this.setState({
+            [type] : value
+        })
+    }
+    goRegister = () => {
+        this.props.history.replace('/register')
+    }
+
+    goMain = () => {
+        this.props.history.replace('/main')
+    }
     render () {
+        const {username,password} = this.state;
         return (
-            <h1>login</h1>
+            <div>
+                <NavBar>硅谷直聘</NavBar>
+                <Logo />
+                <WingBlank>
+                    <List>
+                        <InputItem onChange={val=>this.onChange('username',val)}>用户名:</InputItem>
+                        <WhiteSpace />
+                        <InputItem onChange={val=>this.onChange('password',val)}>密&nbsp;&nbsp;&nbsp;码:</InputItem>
+                        <WhiteSpace />
+                    </List>
+                    <Button type="primary" onClick={this.goMain}>登陆</Button>
+                    <WhiteSpace />
+                    <Button onClick={this.goRegister}>还没有账户</Button>
+                </WingBlank>
+            </div>
         )
     }
 }
--- gzhipin-client/src/components/register/index.jsx
+++ gzhipin-client/src/components/register/index.jsx
@@ -1,11 +1,61 @@
 import React, {Component} from 'react';
+import {NavBar,WingBlank,WhiteSpace,List,InputItem,Radio,Button} from 'antd-mobile';
+import Logo from '../logo'
+
+const Item = List.Item;
 
 
 class Register extends Component {
+    state = {
+        laoban:true,
+        username:'',
+        password:'',
+        rePassword:'',
+
+    }
+
+    handleChange = (type,value) => {
+        this.setState({
+            [type]:value
+        })
+    }
+
+    register = () => {
+        const {laoban,password,rePassword,username} = this.state;
+        console.log(laoban,password,rePassword,username);
+    }
+
+    goLogin = () => {
+
+        this.props.history.replace('/login');
+    }
+
     render () {
+        const {laoban} = this.state;
         return (
             <div>
-               
+                <NavBar>硅谷直聘</NavBar>
+                <Logo />
+                <WingBlank>
+                    <List>
+                       <InputItem onChange={val=>this.handleChange('username',val)}>用户名:</InputItem>
+                        <WhiteSpace />
+                       <InputItem onChange={val=>this.handleChange('password',val)}>密&nbsp;&nbsp;&nbsp;码:</InputItem>
+                        <WhiteSpace />
+                        <InputItem onChange={val=>this.handleChange('rePassword',val)}>确认密码:</InputItem>
+                        <WhiteSpace />
+                        <Item>
+                            用户类型:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <Radio checked={!laoban} onChange={this.handleChange.bind(null,'laoban',false)}>大神</Radio>&nbsp;&nbsp;&nbsp;
+                            <Radio checked={laoban} onChange={this.handleChange.bind(null,'laoban',true)}>老板</Radio>
+                        </Item>
+                        <WhiteSpace />
+                        <Button type="primary" onClick={this.register}>注册</Button>
+                        <WhiteSpace />
+                        <Button onClick={this.goLogin}>已有账户</Button>
+
+                    </List>
+                </WingBlank>
             </div>
         )
     }
